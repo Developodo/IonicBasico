45c26e222b2c0e067a50a64c3d3f11e174cf25fc
Carlos CSS. Serrano <pcserrano@ciclos.local>
2022-12-14 11:18:32 +0100

    b

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Camera, CameraResultType } from '@capacitor/camera';
+import { IonImg } from '@ionic/angular';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
-
+  @ViewChild('foto') foto:IonImg;
   constructor() {}
+
+  public async hazfoto(){
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Uri,
+
+    });
+  
+    // image.webPath will contain a path that can be set as an image src.
+    // You can access the original file using image.path, which can be
+    // passed to the Filesystem API to read the raw data of the image,
+    // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+    let imageUrl = image.webPath;
+  
+    // Can be set to the src of an image now
+
+    this.foto.src = imageUrl;
+  }
 
 }
